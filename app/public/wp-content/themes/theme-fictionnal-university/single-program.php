25c2cead2b8b64864783ffcf8d40045f2b02cb9e
jaillardsolene <jaillard.solene@gmail.com>
2018-08-30 14:42:41 +0200

    Liaison front end (debut)

--- app/public/wp-content/themes/theme-fictionnal-university/single-program.php
+++ app/public/wp-content/themes/theme-fictionnal-university/single-program.php
@@ -14,21 +14,12 @@
 
       <div class="container container--narrow page-section">
       <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true">
-      </i> Event home</a>
+      <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true">
+      </i> All programs</a>
       <span class="metabox__main"><?php the_title(); ?></span></p>
     </div>
       <div class="generic-content"><?php the_content(); ?></div>
-
-  <?php 
-  $relatedPrograms = get_field('related_programs');
-  echo '<ul class>';
-  foreach($relatedPrograms as $program) { ?>
-    <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
- <?php }
- echo '</ul>';
-   ?>      
-   </div>
+      </div>
  <?php }
  get_footer();
 ?>
