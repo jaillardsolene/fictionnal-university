9f08e9a1b40d659a34911479c4fc7f6e2a6d1598
so <solene.jaillard@itsfactory.fr>
2018-09-21 14:02:02 +0200

    Campuses continued

--- app/public/wp-content/themes/theme-fictionnal-university/archive-program.php
+++ app/public/wp-content/themes/theme-fictionnal-university/archive-program.php
@@ -1,22 +1,29 @@
 <?php
+
 get_header();
 pageBanner(array(
-    'title' => 'All Programs',
-    'subtitle'=> 'There is something for everyone. Have a look around.'
-)) ?>
+  'title' => 'All Programs',
+  'subtitle' => 'There is something for everyone. Have a look around.'
+));
+ ?>
 
-   <div class="container container--narrow page-section">
-    <ul class="link-list min-list">
-    <?php
-  while (have_posts()){
+<div class="container container--narrow page-section">
+
+<ul class="link-list min-list">
+
+<?php
+  while(have_posts()) {
     the_post(); ?>
     <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-    <?php }
-    echo paginate_links();
-  ?>
-  </ul>
-  </div>
-
-<?get_footer();
+  <?php }
+  echo paginate_links();
 ?>
+</ul>
 
+
+
+</div>
+
+<?php get_footer();
+
+?>
--- app/public/wp-content/themes/theme-fictionnal-university/page-past-events.php
+++ app/public/wp-content/themes/theme-fictionnal-university/page-past-events.php
@@ -1,43 +1,42 @@
 <?php
+
 get_header();
 pageBanner(array(
-	'title' => 'Past Events',
-	'subtitle' => 'Recap of our past events.'
-)) ?>
+  'title' => 'Past Events',
+  'subtitle' => 'A recap of our past events.'
+));
+ ?>
 
+<div class="container container--narrow page-section">
 <?php
-echo '<div class="container container--narrow page-section">';
-$today = date('Ymd');
-$pastEvents = new WP_Query(array(
-	'paged'=>get_query_var('paged', 1),
-	'post_type' => 'event',
-	'meta_key' => 'event_date',
-	'orderby' => 'meta_value_num',
-	'odrer' =>'ASC',
-	'meta_query' => array(
-		array(
-			'key'=> 'event_date',
-			'compare'=> '<',
-			'value'=> $today,
-			'type' => 'numeric'
-		)
-	)
-));
+  
+  $today = date('Ymd');
+  $pastEvents = new WP_Query(array(
+    'paged' => get_query_var('paged', 1),
+    'post_type' => 'event',
+    'meta_key' => 'event_date',
+    'orderby' => 'meta_value_num',
+    'order' => 'ASC',
+    'meta_query' => array(
+      array(
+        'key' => 'event_date',
+        'compare' => '<',
+        'value' => $today,
+        'type' => 'numeric'
+      )
+    )
+  ));
 
-
-while ($pastEvents->have_posts()) {
-	$pastEvents->the_post();
-	get_template_part('template-parts/content-event');
-}
-echo paginate_links(array(
-	'total'=> $pastEvents->max_num_pages
-));
-echo '</div>';
+  while($pastEvents->have_posts()) {
+    $pastEvents->the_post(); 
+    get_template_part('template-parts/content-event');
+  }
+  echo paginate_links(array(
+    'total' => $pastEvents->max_num_pages
+  ));
 ?>
+</div>
 
+<?php get_footer();
 
-<?get_footer();
 ?>
-
-
-
